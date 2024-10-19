1efe0324b5c23cc729cdfa857e8d5cdc5f989696
JJLibra <2565474095@qq.com>
2024-10-19 21:47:09 +0800

    🐞 fix: no sudo

--- zsh-eza.sh
+++ zsh-eza.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or with sudo."
-  exit 1
-fi
-
 echo "Installing essential Zsh plugins..."
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -14,14 +9,28 @@ fi
 
 if ! command -v eza &> /dev/null; then
   echo "Installing eza..."
-  apt install -y gpg
-  mkdir -p /etc/apt/keyrings
-  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | tee /etc/apt/sources.list.d/gierens.list
-  chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
-  apt update && apt install -y eza
+  sudo apt install -y gpg
+  sudo mkdir -p /etc/apt/keyrings
+  wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+  echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+  sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+  sudo apt update
+  sudo apt install -y eza
 else
   echo "eza already installed, skipping..."
 fi
+
+echo "Adding additional plugin configuration to .zshrc..."
+cat << 'EOF' >> "$HOME/.zshrc"
+
+alias c="clear"
+
+# Alias for ls replacement: eza
+alias ls="eza --icons"
+alias ll="eza --icons --long --header"
+alias la="eza --icons --long --header --all"
+alias lg="eza --icons --long --header --all --git"
+alias tree="eza --icons --tree"
+EOF
 
 echo "Installation complete! Please restart your terminal or log out and back in to apply the changes."
--- zsh-zoxide.sh
+++ zsh-zoxide.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or with sudo."
-  exit 1
-fi
-
 echo "Installing Zsh plugins(zoxide)..."
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
@@ -13,6 +8,14 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
 fi
 
 echo "Installing zoxide..."
-apt install -y zoxide fzf
+sudo apt install -y zoxide fzf
+
+echo "Adding additional plugin configuration to .zshrc..."
+cat << 'EOF' >> "$HOME/.zshrc"
+
+# Alias for cd replacement: zoxide
+eval "$(zoxide init --cmd cd zsh)"
+alias cd=z
+EOF
 
 echo "Installation complete! Please restart your terminal or log out and back in to apply the changes."
