a25cc248ae61e7d8ed22f7434986af9db020c3a6
JJLibra <2565474095@qq.com>
2024-10-19 21:23:07 +0800

    🐞 fix: zsh-plugins.sh

--- zsh-plugins.sh
+++ zsh-plugins.sh
@@ -1,10 +1,5 @@
 #!/bin/zsh
 
-if [ "$EUID" -ne 0 ]; then
-  echo "Please run this script as root or with sudo."
-  exit 1
-fi
-
 echo "Installing essential Zsh plugins..."
 
 if [ ! -d "$HOME/.oh-my-zsh" ]; then
